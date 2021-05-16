aaf0e60789ffdc62202e2e016bdde065e5f1f9e2
Minwoo Park <positive1attraction@gmail.com>
2021-05-16 11:41:07 +0900

    initial Settings

---
+++ lecture/back/index.ts
@@ -0,0 +1,12 @@
+import * as express from "express";
+
+const app = express();
+const prod = process.env.NODE_ENV === "production";
+
+app.get("/", (req, res) => {
+  res.send("react nodebird 백엔드 정상 작동");
+});
+
+app.listen(prod ? process.env.PORT : 3065, () => {
+  console.log(`server is running on ${process.env.PORT}`);
+});
